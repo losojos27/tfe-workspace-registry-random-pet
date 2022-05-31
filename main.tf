5466474d2e3f7ade51b356a6c3bd07617d99939e
Lee Owensby <lee.owensby@gmail.com>
2022-05-31 10:37:06 -0500

    declaring variables

--- main.tf
+++ main.tf
@@ -6,6 +6,18 @@ terraform {
   }
 }
 
+variable "pet_name_length" {
+  type        = number
+  description = "Number of words in pet name"
+  default     = 2
+}
+
+variable "animal_name_length" {
+  type        = number
+  description = "Number of words in animal name"
+  default     = 5
+}
+
 module "pet" {
   source  = "app.terraform.io/lo-petgrackle/pet/random"
   version = "1.2.1"
