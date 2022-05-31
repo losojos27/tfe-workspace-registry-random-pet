43cff9a5e7bc82abc3765a06bc030f54e644e427
Lee Owensby <lee.owensby@gmail.com>
2022-05-31 10:21:15 -0500

    ..

--- main.tf
+++ main.tf
@@ -1,21 +1,13 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
-
 module "pet" {
   source  = "app.terraform.io/lo-petgrackle/pet/random"
-  version = "1.2.0"
+  version = "1.2.1"
 
   pet_name_length = var.pet_name_length
 }
 
 module "animal" {
   source  = "app.terraform.io/lo-petgrackle/pet/random"
-  version = "1.2.0"
+  version = "1.2.1"
 
   pet_name_length = var.animal_name_length
 }
