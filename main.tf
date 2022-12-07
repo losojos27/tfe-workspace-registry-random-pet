1334893eb641958926c2779e51db445a76edd3da
Lee Owensby <lee.owensby@gmail.com>
2022-12-07 16:12:32 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ module "pet" {
   source  = "app.terraform.io/lo-petgrackle/pet/random"
   version = "1.0.0"
 
-  pet_name_length = var.pet_name_length
+  #pet_name_length = var.pet_name_length
 }
 
 module "animal" {
   source  = "app.terraform.io/lo-petgrackle/pet/random"
   version = "1.0.0"
 
-  pet_name_length = var.animal_name_length
+ # pet_name_length = var.animal_name_length
 }
